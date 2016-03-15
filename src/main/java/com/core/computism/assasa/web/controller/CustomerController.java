59bfcf4c546415e347e57062f8d7cb79a84ece52
mustafa.amin <mustafa.amin@venturedive.com>
2016-03-15 16:33:47 +0500

    ngTable added

--- src/main/java/com/core/computism/assasa/web/controller/CustomerController.java
+++ src/main/java/com/core/computism/assasa/web/controller/CustomerController.java
@@ -3,12 +3,15 @@ package com.core.computism.assasa.web.controller;
 import com.core.computism.assasa.inventory.domain.CustomerDto;
 import com.core.computism.assasa.exception.PosBusinessException;
 import com.core.computism.assasa.inventory.service.CustomerService;
+import com.core.computism.assasa.persistence.entity.inventory.Customer;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by VD on 2/28/2016.
@@ -17,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "customer/")
 public class CustomerController extends BaseController {
 
-    private static final Logger LOGGER = Logger.getLogger(SupplierController.class);
+    private static final Logger LOGGER = Logger.getLogger(CustomerController.class);
 
     @Autowired
     CustomerService customerService;
@@ -26,5 +29,12 @@ public class CustomerController extends BaseController {
     public Long add(@RequestBody CustomerDto customerDto) throws PosBusinessException {
         LOGGER.info("Adding item ...");
         return customerService.add(customerDto);
+    }
+
+    @RequestMapping(value = "list",method = RequestMethod.GET)
+    public List<CustomerDto> listCustomers() throws PosBusinessException {
+        LOGGER.info("Adding item ...");
+        return customerService.getCustomers();
+
     }
 }
