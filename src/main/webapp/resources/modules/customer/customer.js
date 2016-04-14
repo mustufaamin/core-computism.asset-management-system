95738f3a318f8bfc31db120b97836e9b7344ba10
muhammad.umer <umermuhammadkhan@gmail.com>
2016-04-15 00:52:37 +0500

    Tweeks in Customer panel in list and add functionality

--- src/main/webapp/resources/modules/customer/customer.js
+++ src/main/webapp/resources/modules/customer/customer.js
@@ -19,12 +19,14 @@
     });
 
     angular.module('Asasa')
-        .controller('CustomerController', ['CustomerGatewayService','$http', function(custGatewaySrv, $http){
+        .controller('CustomerController', ['CustomerGatewayService', 'CountryService', 'CityService', '$http', function(custGatewaySrv, countrySrv, citySrv, $http){
             var custCtrl = this;
             custCtrl.showPanel = false;
             custCtrl.isView = false;
             custCtrl.slidePanelHeading = '';
             custCtrl.listCustomer = [];
+            custCtrl.listCountry = [];
+            custCtrl.listRelevantCities = [];
 
             custCtrl.getCustomerList = function(){
                 custCtrl.listCustomer = [];
@@ -46,16 +48,73 @@
                     }
                 });
             };
-            custCtrl.getCustomerList();
 
-            custCtrl.addCustomer = function (){
-                var customer = {};
-                custGatewaySrv.addCustomer().$promise.then(function(response){
+            custCtrl.getCountries = function(){
+                custCtrl.listCountry = [];
+                countrySrv.getCountriesList().$promise.then(function(response){
+                    if(response != null){
+                        for(var i = 0; i < response.data.length; i++){
+                            var country = {};
+                            country.id = response.data[i].id;
+                            country.name = response.data[i].name;
 
+                            custCtrl.listCountry.push(country);
+                        }
+                    }
                 });
             };
 
-            custCtrl.openCustomerSlidePanel = function(type){
+            custCtrl.getCustomerList();
+            custCtrl.getCountries();
+
+            custCtrl.onSelectCountry = function(country){
+                if(country != null){
+                    if(country.id != null){
+                        custCtrl.listRelevantCities = [];
+                        citySrv.getRelevantCities({countryId:country.id}).$promise.then(function(response){
+                            if(response != null){
+                                for(var i = 0; i < response.data.length; i++){
+                                    var city = {};
+                                    city.id = response.data[i].id;
+                                    city.name = response.data[i].name;
+                                    city.state = response.data[i].state;
+                                    city.countryId = response.data[i].country_id;
+                                    custCtrl.listRelevantCities.push(city);
+                                }
+                            }
+                        });
+                    }
+                }
+            };
+
+            custCtrl.addCustomer = function (){
+                var customer = {};
+                customer.firstName = custCtrl.firstName;
+                customer.lastName = custCtrl.lastName;
+                customer.phoneNumber = custCtrl.phone;
+                customer.mobileNumber = custCtrl.mobile;
+                customer.email = custCtrl.email;
+                customer.locationAddress = custCtrl.address;
+                customer.cityId = custCtrl.city.id;
+                customer.city = custCtrl.city.name;
+                custGatewaySrv.addCustomer(customer).$promise.then(function(response){
+                    if(response){
+
+                    }
+                });
+            };
+
+            custCtrl.openCustomerSlidePanel = function(type, customer){
+                if(customer != null){
+                    custCtrl.firstName = customer.firstName;
+                    custCtrl.lastName = customer.lastName;
+                    custCtrl.phone = customer.phoneNumber;
+                    custCtrl.mobile = customer.mobileNumber;
+                    custCtrl.email = customer.email;
+                    custCtrl.address = customer.locationAddress;
+                    custCtrl.city = custCtrl.getCityModel(customer.cityId);
+                    custCtrl.country = custCtrl.getCountryModel(customer.countryId);
+                }
                 custCtrl.showPanel = true;
                 custCtrl.isView = false;
                 if(type == 1){
@@ -65,6 +124,22 @@
                     custCtrl.isView = true;
                 }else{
                     custCtrl.slidePanelHeading = "Edit Customer";
+                }
+            };
+
+            custCtrl.getCountryModel = function(countryId){
+                for(var i = 0; i < custCtrl.listCountry.length; i++){
+                    if(custCtrl.listCountry[i].id == countryId){
+                        return custCtrl.listCountry[i];
+                    }
+                }
+            };
+
+            custCtrl.getCityModel = function(cityId){
+                for(var i = 0; i < custCtrl.listRelevantCities.length; i++){
+                    if(custCtrl.listRelevantCities[i].id == cityId){
+                        return custCtrl.listRelevantCities[i];
+                    }
                 }
             };
     }]);
@@ -79,16 +154,29 @@
                 listCustomer: {method: 'GET', isArray: false, url: '/customer/list'},
 
                 addCustomer: {method: 'POST', isArray: false, url: '/customer/add'}
-                /*addItem: {method: 'POST',isArray: false,url:'item/:add'},
-                addItemType: {method: 'POST',isArray: false,url:'/item/addType/:typeName'},
-
-                *//*Supplier Methods*//*
-                addSupplier:{method: 'POST',isArray: false,url:'/supplier/add'},
-
-                *//*Customer Methods*//*
-                addCustomer:{method: 'POST',isArray: false,url:'/customer/add'}*/
             });
     }]);
+
+    angular.module('Asasa')
+        .service('CountryService',['$resource' ,function ($resource) {
+            var countrySrv = this;
+            return $resource('',{},
+                {
+                    getCountriesList: {method: 'GET', isArray: false, url: '/country/getAll'}
+
+                });
+        }]);
+
+    angular.module('Asasa')
+        .service('CityService',['$resource' ,function ($resource) {
+            var citySrv = this;
+            return $resource('',{},
+                {
+                    getRelevantCities: {method: 'GET', isArray: false, url: '/city/getCities/:countryId'}
+
+                });
+        }]);
+
 
 
 })();
