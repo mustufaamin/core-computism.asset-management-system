23156a055c03eb52463c15f78bf5d691fff171b7
muhammad.umer <umermuhammadkhan@gmail.com>
2016-05-13 21:17:24 +0500

    Payment Type response changes

--- src/main/java/com/core/computism/assasa/web/controller/ar/PaymentController.java
+++ src/main/java/com/core/computism/assasa/web/controller/ar/PaymentController.java
@@ -6,6 +6,8 @@ import com.core.computism.assasa.ar.dto.PaymentTypeDto;
 import com.core.computism.assasa.ar.service.PaymentService;
 import com.core.computism.assasa.ar.service.PaymentTypeService;
 import com.core.computism.assasa.exception.ArBusinessException;
+import com.core.computism.assasa.util.ServerResponse;
+import com.core.computism.assasa.web.controller.BaseController;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,13 +20,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by VD on 3/6/2016.
  */
 @Controller(value = "paymentController")
 @RequestMapping(value = "/payments/", produces = {MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8"})
-public class PaymentController {
+public class PaymentController extends BaseController {
 
     private static final Logger LOGGER = Logger.getLogger(PaymentController.class);
 
@@ -35,44 +38,45 @@ public class PaymentController {
     PaymentTypeService paymentTypeService;
 
     @RequestMapping(value = "addPayment", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public void addPayment(@RequestBody PaymentDto paymentDto) throws ArBusinessException {
+    public Map<String, Object> addPayment(@RequestBody PaymentDto paymentDto) throws ArBusinessException {
         try {
             paymentService.savePayment(paymentDto);
+            return createSuccessModelMap();
         } catch (ArBusinessException e) {
-            LOGGER.error(e);
+            throw new ArBusinessException(e);
         }
     }
 
     @RequestMapping(value = "saveNPostPayment", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public void saveNPostPayment(@RequestBody PaymentDto paymentDto) throws ArBusinessException {
+    public Map<String, Object> saveNPostPayment(@RequestBody PaymentDto paymentDto) throws ArBusinessException {
         try {
             paymentService.saveNPostPayment(paymentDto);
+            return createSuccessModelMap();
         } catch (ArBusinessException e) {
-            LOGGER.error(e);
+            throw new ArBusinessException(e);
         }
     }
 
     @RequestMapping(value = "addPaymentType", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public void addPaymentType(@RequestBody PaymentTypeDto paymentTypeDto) throws ArBusinessException {
+    public Map<String, Object> addPaymentType(@RequestBody PaymentTypeDto paymentTypeDto) throws ArBusinessException {
         try {
             paymentTypeService.savePaymentType(paymentTypeDto);
+            return createSuccessModelMap();
         } catch (ArBusinessException e) {
-            LOGGER.error(e);
+            throw new ArBusinessException(e);
         }
     }
 
     @RequestMapping(value = "paymenttypes", method = RequestMethod.GET)
     public @ResponseBody
-    List<PaymentTypeDto> getPaymentTypes() throws ArBusinessException {
+    ServerResponse<List<PaymentTypeDto>> getPaymentTypes() throws ArBusinessException {
         List<PaymentTypeDto> paymentTypeDtos = null;
         try {
             paymentTypeDtos = paymentTypeService.getPaymentTypes();
+            ServerResponse<List<PaymentTypeDto>> response = toResponse(paymentTypeDtos);
+            return response;
         } catch (ArBusinessException e) {
-            LOGGER.error(e);
+            throw new ArBusinessException(e);
         }
-        return paymentTypeDtos;
     }
 }
