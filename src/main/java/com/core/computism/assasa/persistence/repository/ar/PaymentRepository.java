a236f4328132e09451d33bf6882d5847545c64b1
iftikhar.faizan <faizan.iftikhar@venturedive.com>
2016-06-05 13:29:33 +0500

    Payment UI interface

--- src/main/java/com/core/computism/assasa/persistence/repository/ar/PaymentRepository.java
+++ src/main/java/com/core/computism/assasa/persistence/repository/ar/PaymentRepository.java
@@ -2,9 +2,15 @@ package com.core.computism.assasa.persistence.repository.ar;
 
 import com.core.computism.assasa.persistence.entity.ar.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by VD on 3/6/2016.
  */
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query("from Payment p where p.arAccount.customer.id =:customerId")
+    List<Payment> getPaymentByCustomerId(@Param("customerId") Long customerId);
 }
--- src/main/java/com/core/computism/assasa/persistence/repository/ar/PaymentTypeRepository.java
+++ src/main/java/com/core/computism/assasa/persistence/repository/ar/PaymentTypeRepository.java
@@ -2,10 +2,15 @@ package com.core.computism.assasa.persistence.repository.ar;
 
 import com.core.computism.assasa.persistence.entity.ar.PaymentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by VD on 3/15/2016.
  */
 public interface PaymentTypeRepository extends JpaRepository<PaymentType, Long> {
-
+    @Query("from PaymentType p where p.status =:statusId")
+    List<PaymentType> getPaymentTypesByStatus(@Param("statusId") Integer statusId);
 }
