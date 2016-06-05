5ac452cb4002a1a0ee62738e815f01f74798828a
iftikhar.faizan <faizan.iftikhar@venturedive.com>
2016-06-05 14:16:32 +0500

    Payment UI interface

--- src/main/webapp/resources/modules/account-receivable/sub-modules/payments/ar-payment.js
+++ src/main/webapp/resources/modules/account-receivable/sub-modules/payments/ar-payment.js
@@ -3,36 +3,36 @@
  */
 (function(){
     angular.module('Asasa')
-        .directive('arAdjustmentPanel', function(){
+        .directive('arPaymentPanel', function(){
             return{
                 restrict: "E",
-                templateUrl: 'resources/modules/account-receivable/sub-modules/adjustments/adjustment/templates/ar-adjustment-panel.jsp'
+                templateUrl: 'resources/modules/account-receivable/sub-modules/payments/template/ar-payment.jsp'
             }
         });
 
     angular.module('Asasa')
-        .controller('ArAdjustmentController', ['ArAdjustmentGatewayService', '$http', function(adjustmentGatewayService, $http){
-            var arAdjustmentCtrl = this;
-            arAdjustmentCtrl.listBillCodes = [];
-            arAdjustmentCtrl.customerName = "Customer's Name";
+        .controller('ArPaymentController', ['ArPaymentGatewayService', '$http', function(paymentGatewayService, $http){
+            var arPaymentCtrl = this;
+            arPaymentCtrl.listPaymentTypes = [];
+            arPaymentCtrl.customerName = "Customer's Name";
 
-            arAdjustmentCtrl.getBillCodeList = function(){
-                arAdjustmentCtrl.listBillCodes = [];
-                adjustmentGatewayService.listOfBillCodes().$promise.then(function(response){
+            arPaymentCtrl.getPaymentList = function(){
+                arPaymentCtrl.listPaymentTypes = [];
+                adjustmentGatewayService.listOfPaymentTypes().$promise.then(function(response){
                     if(response != null){
                         console.log(response);
                         for(var i = 0; i < response.data.length; i++){
                             var billCode = {};
                             billCode.billCodeId = response.data[i].id;
                             billCode.name = response.data[i].name;
-                            arAdjustmentCtrl.listBillCodes.push(billCode);
+                            arPaymentCtrl.listPaymentTypes.push(billCode);
                         }
                     }
                 });
             };
-            arAdjustmentCtrl.getBillCodeList();
+            arPaymentCtrl.getPaymentList();
 
-            arAdjustmentCtrl.getCustomer = function(val){
+            arPaymentCtrl.getCustomer = function(val){
                 var _url = "/customer/search/" + val;
                 return $http.get(_url).then(function(response){
                     return response.data.data.map(function(item){
@@ -46,51 +46,69 @@
                 });
             };
 
-            arAdjustmentCtrl.onSelectCustomer = function(customer){
-                arAdjustmentCtrl.customerType = customer.customerTypeId;
-                arAdjustmentCtrl.customerStatus = customer.customerStatus;
-                arAdjustmentCtrl.customerName = arAdjustmentCtrl.customer.label;
+            arPaymentCtrl.onSelectCustomer = function(customer){
+                arPaymentCtrl.customerType = customer.customerTypeId;
+                arPaymentCtrl.customerStatus = customer.customerStatus;
+                arPaymentCtrl.customerName = arPaymentCtrl.customer.label;
             };
 
             //TODO: AR Adjustment list
-            arAdjustmentCtrl.getArList = function(){
-                arAdjustmentCtrl.listAr = [];
-                adjustmentGatewayService.listOfArAdjustment().$promise.then(function(response){
+            arPaymentCtrl.getArPaymentList = function(customerId){
+
+                var params = {
+                    customerId:customerId
+                }
+                arPaymentCtrl.listArPayments = [];
+                paymentGatewayService.listOfArPayment().$promise.then(function(response){
                     if(response != null){
                         for(var i = 0; i < response.data.length; i++){
-                            var customer = {};
-                            customer.id = response.data[i].id;
-                            customer.firstName = response.data[i].firstName;
-                            customer.lastName = response.data[i].lastName;
-                            customer.locationAddress = response.data[i].locationAddress;
-                            customer.phoneNumber = response.data[i].phoneNumber;
-                            customer.mobileNumber = response.data[i].mobileNumber;
-                            customer.email = response.data[i].email;
-                            customer.cityId = response.data[i].cityId;
+                            var payment = {};
 
-                            arAdjustmentCtrl.listAr.push(customer);
+                            payment.arAccountId = response.data[i].arAccountId;
+                            payment.referenceArAccountId = response.data[i].referenceArAccountId;
+                            payment.paymentTypeId = response.data[i].paymentTypeId;
+                            payment.paymentAmount = response.data[i].paymentAmount;
+                            payment.paymentDate = response.data[i].paymentDate;
+                            payment.postedDate = response.data[i].postedDate;
+                            payment.processedBy = response.data[i].processedBy;
+                            payment.description = response.data[i].description;
+                            payment.note = response.data[i].note;
+                            payment.status = response.data[i].status;
+                            payment.createdBy = response.data[i].createdBy;
+                            payment.modifiedBy = response.data[i].modifiedBy;
+                            payment.batchId = response.data[i].batchId;
+                            payment.source = response.data[i].source;
+                            payment.suppressOnStatement = response.data[i].suppressOnStatement;
+                            payment.statementDescription2 = response.data[i].statementDescription2;
+                            payment.referenceId = response.data[i].referenceId;
+
+                            arPaymentCtrl.listArPayments.push(payment);
                         }
                     }
                 });
             };
 
-            arAdjustmentCtrl.addArAdjustment = function (){
-                var adjustment = {};
-                adjustment.arAccountId = 1;//arAdjustmentCtrl.arAccountId;
-                adjustment.billCodeId = arAdjustmentCtrl.billCodeId;
-                adjustment.adjustmentDate = arAdjustmentCtrl.adjustmentDate;
-                adjustment.amount = arAdjustmentCtrl.amount;
-                adjustment.adjustmentType = 1;//arAdjustmentCtrl.adjustmentType;
-                adjustment.status = 1;//arAdjustmentCtrl.status;
-                adjustment.batchId = arAdjustmentCtrl.batchId;
-                adjustment.description = arAdjustmentCtrl.description;
-                adjustment.statementDescription2 = arAdjustmentCtrl.statementDescription2;
-                adjustment.note = arAdjustmentCtrl.note;
-                adjustment.suppressOnStatement = arAdjustmentCtrl.suppressOnStatement;
-                adjustment.excludeValueAdded = arAdjustmentCtrl.excludeValueAdded;
-                adjustment.createdBy = 1;//arAdjustmentCtrl.createdBy; TODO : Hardcoded value for UserId.
+            arPaymentCtrl.addArPayment = function (){
+                var payment = {};
+                payment.arAccountId = arPaymentCtrl.arAccountId;
+                //payment.referenceArAccountId = arPaymentCtrl.referenceArAccountId;
+                payment.paymentTypeId = arPaymentCtrl.paymentTypeId;
+                payment.paymentAmount = arPaymentCtrl.paymentAmount;
+                payment.paymentDate = arPaymentCtrl.paymentDate;
+                payment.postedDate = arPaymentCtrl.paymentDate;
+                //payment.processedBy = arPaymentCtrl.processedBy;
+                payment.description = arPaymentCtrl.description;
+                payment.note = arPaymentCtrl.note;
+                payment.status = arPaymentCtrl.status;
+                payment.createdBy = arPaymentCtrl.createdBy;
+                payment.modifiedBy = arPaymentCtrl.modifiedBy;
+                payment.batchId = arPaymentCtrl.batchId;
+                payment.source = arPaymentCtrl.source;
+                payment.suppressOnStatement = arPaymentCtrl.suppressOnStatement;
+                payment.statementDescription2 = arPaymentCtrl.statementDescription2;
+                payment.referenceId = arPaymentCtrl.referenceId;
 
-                adjustmentGatewayService.addArAdjustment(adjustment).$promise.then(function(response){
+                adjustmentGatewayService.addArPayment(payment).$promise.then(function(response){
                     if(response){
 
                     }
@@ -110,15 +128,15 @@
 
 
     angular.module('Asasa')
-        .service('ArAdjustmentGatewayService',['$resource' ,function ($resource) {
-            var arAdjustmentGatewayService = this;
+        .service('ArPaymentGatewayService',['$resource' ,function ($resource) {
+            var arPaymentGatewayService = this;
             return $resource('',{},
                 {
-                    listOfArAdjustment :{method: 'GET', isArray: false, url:"/glAccount/glAccountTypes"},
+                    listOfArPayment :{method: 'GET', isArray: false, url:"/payments/list/:customerId"},
 
-                    listOfBillCodes : {method: 'GET', isArray: false, url: "/billCodes/list"},
+                    listOfPaymentTypes : {method: 'GET', isArray: false, url: "/billCodes/list"},
 
-                    addArAdjustment :{method: 'POST', isArray: false, url:"/adjustments/addAdjustment"}
+                    addArPayment :{method: 'POST', isArray: false, url:"/adjustments/addAdjustment"}
 
                 });
         }]);
